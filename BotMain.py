384fb1cbdb1ec159b89db6cec1e79b14b741fa05
Ben Chen <jinerben@gmail.com>
2017-08-07 13:39:39 +0200

    ignore database file

--- BotMain.py
+++ BotMain.py
@@ -8,26 +8,26 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 # logging.basicConfig(level=logging.DEBUG)
 
 bot = ChatBot(
-    "NoteBot",
-    storage_adapter="chatterbot.storage.SQLStorageAdapter",
-    database_uri="sqlite:///testdatabase.db",
-    logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.BestMatch'
-        },
-        {
-            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
-            'threshold': 0.25,
-            'default_response': 'What?'
-        },
-    ],
-    input_adapter="chatterbot.input.VariableInputTypeAdapter",
-    output_adapter="chatterbot.output.OutputAdapter",
+	"NoteBot",
+	storage_adapter="chatterbot.storage.SQLStorageAdapter",
+	database_uri="sqlite:///testdatabase.db",
+	logic_adapters=[
+		{
+			'import_path': 'chatterbot.logic.BestMatch'
+		},
+		{
+			'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+			'threshold': 0.25,
+			'default_response': 'What?'
+		},
+	],
+	input_adapter="chatterbot.input.VariableInputTypeAdapter",
+	output_adapter="chatterbot.output.OutputAdapter",
 )
 bot.set_trainer(ChatterBotCorpusTrainer)
-bot.read_only=True
+# bot.train("output.out")
+bot.read_only = True
 
 input = '现要求被告立即归还借款30000元'
-#input = {'2007年9月25日，被告向原告借款30000元' : '', '现要求被告立即归还借款30000元' : ''}
 output = bot.get_response(input)
 print(output)
