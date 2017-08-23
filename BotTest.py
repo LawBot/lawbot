a3fef126b14a773f0a32e62b0b97fccba6315a9a
Ben Chen <jinerben@gmail.com>
2017-08-23 17:28:53 +0200

    Added function to generate test report to file

--- BotTest.py
+++ BotTest.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from chatterbot import ChatBot
+from time import localtime, strftime
+from shutil import copyfile
+import os, errno
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from TextProcessor.LabeledCaseProcessor import LabeledCaseProcessor
 
 # Uncomment the following lines to enable verbose logging
 import logging
+
 # logging.basicConfig(level=logging.INFO)
 # logging.basicConfig(level=logging.DEBUG)
 
@@ -17,8 +21,8 @@ bot = ChatBot(
 			'import_path': 'chatterbot.logic.BestMatch'
 		},
 		{
-			'import_path': 'chatterbot.logic.LowConfidenceAdapter',
-			'threshold': 0.25,
+			'import_path'     : 'chatterbot.logic.LowConfidenceAdapter',
+			'threshold'       : 0.25,
 			'default_response': 'What?'
 		},
 	],
@@ -30,34 +34,65 @@ bot.set_trainer(ChatterBotCorpusTrainer)
 bot.read_only = True
 
 # Uncommenting the following lines to make tests and produce test results
-TEST_PATH = 'output\\test_6818.txt'
+time_folder = strftime("%Y-%m-%d_%H-%M-%S", localtime())
+TEST_INPUT_PATH = 'output\\test_6818.txt'
+TEST_RESULT_FOLDER = 'TestResults\\' + time_folder
+TEST_RESULT_FILE = TEST_RESULT_FOLDER + '\\TestResult.txt'
+
+if not os.path.exists(TEST_RESULT_FOLDER):
+	os.makedirs(TEST_RESULT_FOLDER)
+
+input_file_name = TEST_INPUT_PATH
+if TEST_INPUT_PATH.rfind('\\') != -1:
+	input_file_name = TEST_INPUT_PATH[TEST_INPUT_PATH.rfind('\\'):]
+
+copyfile(TEST_INPUT_PATH, TEST_RESULT_FOLDER + '\\' + input_file_name)
+
+test_result_str = ''
+tmp_str = ''
+
 lcp = LabeledCaseProcessor('INFO')
-cu_list = lcp.readTestFile(TEST_PATH)
+cu_list = lcp.readTestFile(TEST_INPUT_PATH)
 
 correct = 0
 incorrect = 0
 
 for idx, cu in enumerate(cu_list):
-  input = cu.text
-  factor = cu.factor
-  output = bot.get_response(input)
+	input = cu.text
+	factor = cu.factor
+	output = bot.get_response(input)
 
-  if factor == output.text:
-    correct += 1
-    print("第" + str(idx + 1) + "条回答正确！")
-  else:
-    incorrect += 1
-    print("======================================")
-    print("第" + str(idx + 1) + "条回答错误")
-    print("原文： " + input)
-    print("应输出： " + factor)
-    print("实际输出： " + output.text)
-    print("======================================")
+	if factor == output.text:
+		correct += 1
+		tmp_str = "第" + str(idx + 1) + "条回答正确！\n"
+		print(tmp_str)
+		test_result_str += tmp_str
+	else:
+		incorrect += 1
+		tmp_str = '!' * 100 + '\n' \
+		          + '第' + str(idx + 1) + '条回答错误\n' \
+		          + '原文： ' + input + '\n' \
+		          + '应输出： ' + factor + '\n' \
+		          + '实际输出： ' + output.text + '\n' \
+		          + '!' * 100 + '\n'
+
+		print(tmp_str)
+		test_result_str += tmp_str
 
 total = correct + incorrect
 percent = correct / total * 100
-print("共计测试数量：" + str(total) + " 正确数量：" + str(correct) + " 错误数量：" + str(incorrect) + " 正确率:" + str(percent) + "%")
+tmp_str = '\n\n' + '=' * 100 + '\n'\
+          + '共计测试数量：' + str(total) \
+          + ' 正确数量：' + str(	correct) \
+          + ' 错误数量：' + str(incorrect) \
+          + ' 正确率:' + str(percent) + '%' + '\n'\
+          + '=' * 100
 
+print(tmp_str)
+test_result_str += tmp_str
+
+with open(TEST_RESULT_FILE, encoding='utf-8', mode='w') as f:
+	f.write(test_result_str)
 
 # The following loop will execute each time the user enters input
 # while True:
